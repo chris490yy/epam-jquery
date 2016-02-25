2c87ac409291ead59b904237110092f481f4dc27
Chris Yu <chris_yu@epam.com>
2016-02-25 15:00:33 +0800

    event and animate

--- js/script.js
+++ js/script.js
@@ -53,8 +53,52 @@ $("#form2 button").click(function() {
 
 // handle the mouseover event here
 
+$("#mouse img").mouseover(function() {
+  $("#mouse img").css("width", "40%");
+});
+
+$("#mouse img").mouseout(function() {
+  $("#mouse img").css("width", "20%");
+});
+
+
 // handle the form events here
 
 $("#formEvents form").submit(function(event) {
+   event.preventDefault();
+   console.log($("#inputEmail3").val());
+   console.log($("#inputPassword3").val());
+   console.log($("#formEvents input[type=checkbox]").prop("checked"));
 
+});
+
+$("#animate1 img").mouseover(function() {
+  $("#animate1 img").animate({
+    width:"40%"
+  });
+});
+
+$("#animate2 img").dblclick(function() {
+  //console.log(typeof $("#animate2 img").css("margin-left"));
+  if($("#animate2 img").css("margin-left") === "750px"){
+    $("#animate2 img").animate({
+    "margin-left":"0"
+    });
+  }else {
+    $("#animate2 img").animate({
+    "margin-left":"750px"
+    });
+  }
+});
+
+$("#animate3 img:first").dblclick(function() {
+  $("#animate3 img:first").fadeOut(function() {
+    $("#animate3 img:last").fadeIn();
+  });
+  // $("#animate3 img").animate({
+  //   opacity: 0
+  // }, 1000, function() {
+  //   $(this).attr("src", "images/cat.jpg");
+  //   $(this).animate({ opacity: 1});
+  // })
 });
