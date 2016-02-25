78dabae5d42f235e95057b36fefc6ad2792c8192
Chris Yu <chris_yu@epam.com>
2016-02-25 11:37:07 +0800

    part1 exercise

--- js/script.js
+++ js/script.js
@@ -8,39 +8,47 @@ $("#prepend button").click(function() {
 });
 
 $("#before button").click(function() {
-	
+	$("#before p").before("Yo!");
 });
 
 $("#css1 button").click(function() {
-	
+  $("#css1 p").css("font-size", "50px");
+
 });
 
 $("#css2 button").click(function() {
-	
+  $("#css2 p").css({"font-size":"50px", "color":"yellow"});
+
 });
 
 $("#attr1 button").click(function() {
-	
+  var des = $("#attr1 p a").attr("href");
+  $("#attr1 button").after(des);
 });
 
 $("#attr2 button").click(function() {
+  $("#attr2 p a").attr("href", "http://www.youtube.com");
 
 });
 
 $("#class1 button").click(function() {
-	
+  $("#class1 p").addClass("bg-primary");
 });
 
 $("#class2 button").click(function() {
-
+  var result = $("#class2 p").hasClass("lead");
+  console.log(result);
+  $("#class2 button").after(result);
 });
 
 $("#form1 button").click(function() {
-
+  var value = $("#exampleInputEmail1").val();
+  console.log(value);
+  $("#exampleInputEmail1").after(value);
 });
 
 $("#form2 button").click(function() {
-
+  $("#exampleInputName2").val('Chris');
 });
 
 // handle the mouseover event here
